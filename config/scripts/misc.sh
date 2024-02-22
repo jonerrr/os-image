f59224037417f7a428a5f7866a16541e6bc8c412
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-02-21 20:32:07 -0500

    add restic, resticprofile, and firefox gnome theme

--- config/scripts/misc.sh
+++ config/scripts/misc.sh
@@ -1,2 +1,4 @@
+set -oue pipefail
+
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop
 systemctl enable dconf-update.service
---
+++ config/scripts/resticprofile.sh
@@ -0,0 +1,3 @@
+set -oue pipefail
+
+curl -sfL https://raw.githubusercontent.com/creativeprojects/resticprofile/master/install.sh | sh

4ebbe49577a926ba7afba70c349f8932a582afe6
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-02-25 00:09:27 -0500

    install resticprofile to /usr/local/bin instead

--- config/scripts/resticprofile.sh
+++ config/scripts/resticprofile.sh
@@ -1,3 +1,5 @@
 set -oue pipefail
 
-curl -sfL https://raw.githubusercontent.com/creativeprojects/resticprofile/master/install.sh | sh
+curl -LO https://raw.githubusercontent.com/creativeprojects/resticprofile/master/install.sh
+chmod +x install.sh
+sudo ./install.sh -b /usr/local/bin

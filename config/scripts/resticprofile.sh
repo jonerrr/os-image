6a6887d415a2de13aa07b8a72c65264a96e7dd03
Jonah <73760377+jonerrr@users.noreply.github.com>
2024-02-25 00:28:25 -0500

    fix resticprofile install

--- config/scripts/resticprofile.sh
+++ config/scripts/resticprofile.sh
@@ -2,4 +2,4 @@ set -oue pipefail
 
 curl -LO https://raw.githubusercontent.com/creativeprojects/resticprofile/master/install.sh
 chmod +x install.sh
-sudo ./install.sh -b /usr/local/bin
+sudo ./install.sh -b /usr/bin
